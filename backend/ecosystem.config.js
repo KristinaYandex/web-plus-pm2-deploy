19e7d111d87f38d08f9ada89e0fcc1fdaed8f018
Kristina Ushakova <mramornayaa@yandex.ru>
2024-01-25 11:42:26 +0300

    ecosystem

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -4,6 +4,10 @@ dotenv.config({ path: './.env.deploy' });
 const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPOSITORY, DEPLOY_REF = 'origin/master' } = process.env;
 
 module.exports = {
+  apps : [{
+    name   : "mesto",
+    script : "dist/app.js"
+  }],
   // Настройка деплоя
   deploy: {
     production: {
@@ -12,7 +16,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'post-deploy': 'cd frontend && pwd && npm ci && npm run build',
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 }
--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -4,10 +4,6 @@ dotenv.config({ path: './.env.deploy' });
 const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPOSITORY, DEPLOY_REF = 'origin/master' } = process.env;
 
 module.exports = {
-  apps : [{
-    name   : "mesto",
-    script : "dist/app.js"
-  }],
   // Настройка деплоя
   deploy: {
     production: {
@@ -16,8 +12,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy': 'cd frontend && pwd && npm ci && npm run build',
     },
   },
 }
